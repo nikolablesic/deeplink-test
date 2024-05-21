b0c3a6156a0ae92f50b4abfcd9dd7f162d23b6ac
nikolablesic <58892580+nikolablesic@users.noreply.github.com>
2024-05-21 19:18:25 +0200

    Add files via upload

--- demo-placanje/script.js
+++ demo-placanje/script.js
@@ -1,14 +1,15 @@
 document.getElementById('generateButton').addEventListener('click', function() {
     const phone = document.getElementById('phone').value;
     const account = document.getElementById('account').value;
+    const price = document.getElementById('price').value;
 
-    if (phone && account) {
-        const qrData = `Phone:${phone};Account:${account}`;
+    if (phone && account && price) {
+		const qrLink = `https://nikolablesic.github.io/demo-placanje/payment.html?racun=${account}&iznos=${price}&telefon=${phone}`
         const qrCodeContainer = document.getElementById('qrcode');
         qrCodeContainer.innerHTML = '';
 
         new QRCode(qrCodeContainer, {
-            text: qrData,
+            text: qrLink,
             width: 256,
             height: 256
         });
