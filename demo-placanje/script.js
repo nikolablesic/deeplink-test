181cedc1125a2d02fff4edf0bace6457998832d3
nikolablesic <58892580+nikolablesic@users.noreply.github.com>
2024-05-23 17:30:26 +0200

    Add files via upload

--- demo-placanje/script.js
+++ demo-placanje/script.js
@@ -13,29 +13,6 @@ document.getElementById('generateButton').addEventListener('click', function() {
             height: 256
         });
 
-        const nfcButton = document.getElementById('nfcButton');
-        nfcButton.style.display = 'block';
-
-        nfcButton.addEventListener('click', async () => {
-            try {
-                alert("Start");
-                if ('NDEFWriter' in window) {
-                    const ndef = new NDEFWriter();
-                    await ndef.write(qrData);
-                    alert("NFC tag written successfully!");
-    
-                    // Vibrate the device for feedback
-                    if (navigator.vibrate) {
-                        navigator.vibrate(200); // Vibrate for 200ms
-                    }
-                } else {
-                    alert("Web NFC is not supported in this browser.");
-                }
-            } catch (error) {
-                alert("Error writing to NFC tag:", error);
-            }
-        });
-
     } else {
         alert('Please fill in both fields.');
     }
